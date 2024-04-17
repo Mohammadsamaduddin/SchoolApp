c42cc1afab0cca39772b37b262aed4166a21da81
Mohammadsamaduddin <mohammadsamaduddin0@gmail.com>
2024-04-17 22:00:12 +0530

    Form implemented with personal details

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent {
+  public studentform: FormGroup;
+  constructor() {
+    this.studentform = new FormGroup(
+      {
+        name: new FormControl(),
+        gender: new FormControl(),
+        mobile: new FormControl(),
+        email: new FormControl(),
+        batch: new FormControl()
+      }
+    )
+  }
 
+  onSubmit(){
+    console.log(this.studentform.value);
+    
+  }
 }
