0d4ed483fe6d990493d2ee0e8187d0576af702d3
Mohammadsamaduddin <mohammadsamaduddin0@gmail.com>
2024-04-18 18:03:17 +0530

    Source from radio button implemented

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -7,39 +7,53 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent {
-  public studentform: FormGroup;
-  constructor() {
-    this.studentform = new FormGroup
-      (
-        {
-          name: new FormControl(),
-          gender: new FormControl(),
-          mobile: new FormControl(),
-          email: new FormControl(),
-          batch: new FormControl(),
-          address: new FormGroup
-            (
-              {
-                city: new FormControl(),
-                district: new FormControl(),
-                state: new FormControl(),
-                pin: new FormControl()
-              }
-            ),
-          educations: new FormArray([]),
-          company : new FormGroup(
+  public studentform: FormGroup = new FormGroup
+    (
+      {
+        name: new FormControl(),
+        gender: new FormControl(),
+        mobile: new FormControl(),
+        email: new FormControl(),
+        batch: new FormControl(),
+        address: new FormGroup
+          (
             {
-              name : new FormControl(),
-              location : new FormControl(),
-              package : new FormControl(),
-              offerdate : new FormControl()
-
+              city: new FormControl(),
+              district: new FormControl(),
+              state: new FormControl(),
+              pin: new FormControl()
             }
           ),
-          souceType: new FormControl()
-        }
-      )
+        educations: new FormArray([]),
+        company: new FormGroup(
+          {
+            name: new FormControl(),
+            location: new FormControl(),
+            package: new FormControl(),
+            offerdate: new FormControl()
 
+          }
+        ),
+        sourcetype: new FormControl(),
+      }
+    )
+  constructor() {
+    this.studentform.get('sourcetype')?.valueChanges.subscribe(
+      (data: any) => {
+
+        if (data == 'direct') {
+          //add busFee
+          this.studentform.addControl('sourcefrom', new FormControl());          //remove hostelFee
+          this.studentform.removeControl('referralname');
+        }
+        else {
+          //add hostelFee
+          this.studentform.addControl("referralname", new FormControl());
+          //reomove busFee
+          this.studentform.removeControl("sourcefrom");
+        }
+      }
+    )
   }
   get educationFormArray() {
     return this.studentform.get('educations') as FormArray;
@@ -49,16 +63,16 @@ export class CreateStudentComponent {
     this.educationFormArray.push(
       new FormGroup(
         {
-          qualification : new FormControl(),
-          year : new FormControl(),
-          percentage : new FormControl()
+          qualification: new FormControl(),
+          year: new FormControl(),
+          percentage: new FormControl()
         }
       )
     )
   }
 
-  onDelete(i:number){
-   this.educationFormArray.removeAt(i);
+  onDelete(i: number) {
+    this.educationFormArray.removeAt(i);
   }
 
   onSubmit() {
