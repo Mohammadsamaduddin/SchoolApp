ae6bf44a9edb68379c7a0b10846b38455a57c041
Mohammadsamaduddin <mohammadsamaduddin0@gmail.com>
2024-04-20 00:01:08 +0530

    Filtering implemented

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -24,4 +24,27 @@ export class StudentService {
   getSortedStudents(column: string, order: string): Observable<any> {
     return this._httpClient.get("https://62b9299dff109cd1dc8ca34f.mockapi.io/students?sortBy=" + column + "&order= " + order);
   }
+
+  getFilteredStudents(term: string): Observable<any> {
+    return this._httpClient.get("https://62b9299dff109cd1dc8ca34f.mockapi.io/students?filter=" + term);
+  }
+
+  loadData(queries: any): Observable<any> {
+    let str = '';
+    if (queries.filter) {
+      str += "filter=" + queries.filter + '&';
+    }
+    if (queries.pageNo) {
+      str += "page=" + queries.pageNo + "&";
+      str += "limit=" + queries.limit + "&";
+    }
+    if (queries.sortBy) {
+      str += "sortBy=" + queries.sortBy + "&";
+      str += "order=" + queries.order + "&";
+    }
+    console.log(queries, str);
+
+    return this._httpClient.get("https://62b9299dff109cd1dc8ca34f.mockapi.io/students?" +str);
+
+  }
 }
